b87502bb54f6825b9bb623ce50ea382ca1e44b1b
IlyaKli <ilya.klimkin.0253@gmail.com>
2022-10-06 00:16:39 +0300

    Add shop item activity

---
+++ app/src/main/java/com/ilya/myapplication/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.ilya.myapplication.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ilya.myapplication.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
