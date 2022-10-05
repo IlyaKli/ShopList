b559456f022849934d2c6541507af2510b3409dc
IlyaKli <ilya.klimkin.0253@gmail.com>
2022-10-05 22:27:00 +0300

    Add Rec View

--- app/src/main/java/com/ilya/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/ilya/myapplication/presentation/MainActivity.kt
@@ -5,15 +5,34 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.ilya.myapplication.R
+import com.ilya.myapplication.domain.ShopItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by lazy { ViewModelProvider(this)[MainViewModel::class.java] }
+    private val shopListAdapter by lazy { ShopListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel.shopList.observe(this) {}
+        setRecyclerView()
+
+        viewModel.shopList.observe(this) {
+            shopListAdapter.setShoList(it)
+        }
+
+        shopListAdapter.shopItemLongClickListener = object : ShopListAdapter.OnShopItemLongClickListener{
+            override fun onLongClick(shopItem: ShopItem) {
+                viewModel.editShopList(shopItem)
+            }
+        }
+    }
+
+    fun setRecyclerView() {
+        shopItemListRecyclerView.adapter = shopListAdapter
+        shopItemListRecyclerView.recycledViewPool.setMaxRecycledViews(ShopListAdapter.ENABLED_VIEW, ShopListAdapter.MAX_POOL_SIZE)
+        shopItemListRecyclerView.recycledViewPool.setMaxRecycledViews(ShopListAdapter.DISABLED_VIEW, ShopListAdapter.MAX_POOL_SIZE)
     }
 }
---
+++ app/src/main/java/com/ilya/myapplication/presentation/ShopListAdapter.kt
@@ -0,0 +1,80 @@
+package com.ilya.myapplication.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ilya.myapplication.R
+import com.ilya.myapplication.domain.ShopItem
+import kotlinx.android.synthetic.main.enabled_shop_item.view.*
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopListViewHolder>() {
+
+    var shopItemLongClickListener: OnShopItemLongClickListener? = null
+
+    var shopList = listOf<ShopItem>()
+
+    fun setShoList(shopList: List<ShopItem>) {
+        this.shopList = shopList
+        notifyDataSetChanged()
+    }
+
+    class ShopListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemTextView = itemView.shopItemTextView
+
+        val countTextView = itemView.countShopItemTextView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopListViewHolder {
+        val view = when (viewType) {
+            ENABLED_VIEW -> LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.enabled_shop_item, parent, false)
+            DISABLED_VIEW -> LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.disabled_shop_item, parent, false)
+            else -> throw RuntimeException("Unknown view type")
+        }
+        return ShopListViewHolder(view)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val shopItem = shopList[position]
+
+        val viewType = when (shopItem.used) {
+            true -> ENABLED_VIEW
+            else -> DISABLED_VIEW
+        }
+
+        return viewType
+    }
+
+    override fun onBindViewHolder(holder: ShopListViewHolder, position: Int) {
+        var shopItem = shopList[position]
+
+        holder.itemTextView.text = shopItem.name
+
+        holder.countTextView.text = shopItem.count.toString()
+
+        holder.itemView.setOnLongClickListener {
+            shopItemLongClickListener?.onLongClick(shopItem)
+            true
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return shopList.size
+    }
+
+    interface OnShopItemLongClickListener {
+        fun onLongClick(shopItem: ShopItem)
+    }
+
+    companion object {
+        const val ENABLED_VIEW = 1
+
+        const val DISABLED_VIEW = 0
+
+        const val MAX_POOL_SIZE = 10
+    }
+}
