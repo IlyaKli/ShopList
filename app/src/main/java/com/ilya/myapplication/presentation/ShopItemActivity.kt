c9a095c33778f088774c4f0a1f933bcf8ddc5fdf
IlyaKli <ilya.klimkin.0253@gmail.com>
2022-10-08 00:36:23 +0300

    Add Shop Item Fragment

--- app/src/main/java/com/ilya/myapplication/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/ilya/myapplication/presentation/ShopItemActivity.kt
@@ -3,17 +3,11 @@ package com.ilya.myapplication.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.ilya.myapplication.R
 import com.ilya.myapplication.domain.ShopItem
-import kotlinx.android.synthetic.main.activity_shop_item.*
 
 class ShopItemActivity : AppCompatActivity() {
-
-    private val shopItemViewModel by lazy { ViewModelProvider(this)[ShopItemViewModel::class.java] }
 
     private var screenMode = UNKNOWN_MODE
 
@@ -23,70 +17,18 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         parseIntent()
-        textChangedListener()
-        observerOnSIViewModel()
         launchRightMode()
     }
 
     private fun launchRightMode() {
-        when (screenMode) {
-            MODE_ADD -> launchAddMode()
-            MODE_EDIT -> launchEditMode()
+        val fragment = when (screenMode) {
+            MODE_ADD -> ShopItemFragment.newInstanceAddMode()
+            MODE_EDIT -> ShopItemFragment.newInstanceEditMode(shopItemId)
+            else -> throw RuntimeException("Unknown screen mode")
         }
-    }
-
-    private fun textChangedListener() {
-        nameTextInputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                shopItemViewModel.resetErrorInputName()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-        })
-
-        countTextInputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                shopItemViewModel.resetErrorInputCount()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-        })
-    }
-
-    private fun observerOnSIViewModel() {
-        shopItemViewModel.isLoad.observe(this) {
-            finish()
-        }
-
-        shopItemViewModel.errorInputName.observe(this) {
-            if (it) {
-                nameTIL.error = getString(R.string.error_input_name)
-            } else {
-                nameTIL.error = null
-            }
-        }
-
-        shopItemViewModel.errorInputCount.observe(this) {
-            if (it) {
-                countTIL.error = getString(R.string.error_input_count)
-            } else {
-                countTIL.error = null
-            }
-        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.shopItemContainer, fragment)
+            .commit()
     }
 
     private fun parseIntent() {
@@ -105,32 +47,6 @@ class ShopItemActivity : AppCompatActivity() {
                 throw RuntimeException("Param extra shop item id is absent")
             }
             shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
-        }
-    }
-
-    private fun launchAddMode() {
-        saveButton.setOnClickListener {
-            val name = nameTextInputEditText.text.toString()
-            val count = countTextInputEditText.text.toString()
-            shopItemViewModel.addShopItem(name, count)
-        }
-    }
-
-    private fun launchEditMode() {
-        setEditInfo()
-        saveButton.setOnClickListener {
-            val name = nameTextInputEditText.text.toString()
-            val count = countTextInputEditText.text.toString()
-            shopItemViewModel.editShopItem(name, count)
-        }
-
-    }
-
-    private fun setEditInfo() {
-        shopItemViewModel.getShopItem(shopItemId)
-        shopItemViewModel.shopItem.observe(this) {
-            nameTextInputEditText.setText(it?.name)
-            countTextInputEditText.setText(it?.count.toString())
         }
     }
 
