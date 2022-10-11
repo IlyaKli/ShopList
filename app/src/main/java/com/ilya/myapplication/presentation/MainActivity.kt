9333b32fbc59ada4510d9aa2efa4a03a50cb6cb7
IlyaKli <ilya.klimkin.0253@gmail.com>
2022-10-12 00:33:51 +0300

    Final (refactoring)

--- app/src/main/java/com/ilya/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/ilya/myapplication/presentation/MainActivity.kt
@@ -44,15 +44,6 @@ class MainActivity() : AppCompatActivity(), ShopItemFragment.OnEditingFinishedLi
         }
     }
 
-    override fun onEditingFinished() {
-        Toast.makeText(
-            this@MainActivity,
-            "Готово",
-            Toast.LENGTH_SHORT
-        ).show()
-        supportFragmentManager.popBackStack()
-    }
-
     private fun setOnButtonClickListener() {
         addShopItemButton.setOnClickListener {
             if (isHorizontalOrientation()) {
